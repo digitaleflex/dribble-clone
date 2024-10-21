f4a724e0b52f8550faa1c1a55245d7e0a247dada
digitaleflex <eurinhash@proton.me>
2024-10-21 14:09:26 +0100

    section 2

--- app/(portal)/page.tsx
+++ app/(portal)/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Section1 from '../../components/sections/section1';
+import Section2 from '@/components/sections/section2';
 
 const page = () => {
   return (
     <>
       <Section1 />
+
+      <Section2 />
     </>
   )
 }
